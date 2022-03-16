c5831df63bb73f8db56f5e6ddce5d9cd85c0267a
Shoaib7889 <shoaib4030891@gmail.com>
2022-03-16 11:46:33 +0500

    reuseable code in dummy

--- src/pages/queries/dummy.js
+++ src/pages/queries/dummy.js
@@ -53,3 +53,31 @@
 //   cache: new InMemoryCache(),
 //   link,
 // });
+
+// --------------
+
+/**
+ * import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+
+const httpLink = new HttpLink({ uri: 'https://api.example.com/graphql' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = localStorage.getItem('auth_token');
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
+  cache: new InMemoryCache()
+});
+ */
